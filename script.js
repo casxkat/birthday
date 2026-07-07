b3aaa8148a305c77f42cb19637c4814f30ee7b1d
casxkat <callistocaspian@gmail.com>
2026-07-07 17:27:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,14 @@
       id: 'first-impression',
       title: 'my first love',
       top: '18%', left: '14%',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt.',
+      text: 'I spent so long not knowing what love meant
+         Or if I'd ever really feel it too.
+Then life, in all its strange coincidence,
+Decided I should find it all in you.
+   No soul before had ever felt like home,
+No name could quiet every fear I knew.
+If hearts are constellations left to roam,
+Then every star has always led to you.',
       image: 'photo1.jpg'
     },
     {
